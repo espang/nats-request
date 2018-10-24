3be009a5aed92506d5e2af6a1d625783604dc869
espang <eike.spang@gmail.com>
2018-10-24 17:35:18 +0100

    add 'stubs' for requests

--- src/nats_request/core.clj
+++ src/nats_request/core.clj
@@ -1,6 +1,47 @@
-(ns nats-request.core)
+(ns nats-request.core
+(:import [rpc Rpc$Request
+          io.nats.client MessageHandler]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare request-chunked request-single)
+
+(def ^:private default-options
+  {:timeout 1
+   :chunksize 64
+   :max-msg-size 768000})
+
+(defn handle [conn path handler]
+  (let [dispatcher (.createDispatcher conn handler)]
+    (.subscribe dispatcher path)))
+
+(defn request [conn path payload & [opt]]
+  (let [{:keys [timeout chunksize max-msg-size]} (merge default-options opt)]
+    (if (> (count payload) max-msg-size)
+      (request-chunked [conn path payload chunksize])
+      (request-single  [conn path payload]))))
+
+
+(defn ^:private request-single
+  [conn path payload]
+  ; describe to 'reuest-scoped-topic'
+  ; send {:request correlation-id
+  ;       :response-inbox unique-topic
+  ;       :payload payload}
+  ; wait 'timeout' seconds for response
+  (println "request-single-msg called")) 
+
+(defn ^:private request-chunked
+  "Sends a request in chunks of size 'chunksize'.
+   The handler should get 1 request on the main
+   path per request. A chunked request will send
+   an initial request containing the inbox for the
+   response. The response-inbox will get a message
+   with the request-inboxes topic.
+  "
+  [conn path payload chunksize]
+  ; describe to 'request-scoped-topic'
+  ; send {:request correlation-id :response-inbox unique-topic}
+  ; wait for {:request-inbox}
+  ; send chunks to 'request-inbox'
+  ; wait 'timeout' seconds for response
+  (println "request-chunked-msg called"))
+
